454a8a8304845c72a959bc559a1abed0b0f610e3
janeffer945 <janeffernjeri945@gmail.com>
2022-03-28 11:11:31 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-  $("#show").click(function(){
-    $("submmitg").show();
-  });
+function myfunction(){
+  var score=0;
+  if (document.getElementById("correct1").checked)
+  {
+      score++;
+  }
+  if (document.getElementById("correct2").checked)
+  {
+      score++;
+  }
+  if (document.getElementById("correct3").checked)
+  {
+      score++;
+  }
+  if (document.getElementById("correct4").checked)
+  {
+      score++;
+  }
+  if (document.getElementById("correct5").checked)
+  {
+      score++;
+  }document.getElementById("scorep").innerHTML = score
+}
+var customerName = prompt("Please enter your name", "<name goes here>");
+if (customerName!= null) {
+    document.getElementById("welcome").innerHTML =
+    "Hello " + customerName + "! How are you today?";
+}
